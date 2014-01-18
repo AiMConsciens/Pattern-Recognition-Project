4dc76655c5f8011edb2eb7e0bf1c69ed3e238e71
Tom Runia <tomrunia@gmail.com>
2014-01-18 15:47:48 +0100

    Added evaluation script using nist_eval and delete old filess

--- im_moment.m
+++
@@ -1,7 +0,0 @@
-function M = im_moment(im, p, q)
-    
-    % m_pq in the paper
-    M = sum(sum(((1:size(im,1))'.^p * (1:size(im,2)).^q) .* im));
-
-end
-
---
+++ scenario2_evaluate.m
@@ -0,0 +1,32 @@
+clc;
+clear;
+delfigs;
+prwaitbar off;
+
+nist_data = prnist(0:9,1:1000);
+
+%% NIST EVAL
+
+clc;
+
+iter = 2;      % Number of performance evaluations
+num_test = 50;  % Number of test objects per class
+
+errors = zeros(1:iter, 1);
+
+for i = 1:iter
+    % Generate a random training set with 10 objects per class 
+    n_trn = gendat(nist_data, 0.01);
+    % Calculate trainings prdataset object
+    trn = my_rep2(n_trn);
+    
+    % Train SVC classifier
+    w = svc(trn);
+    
+    % Evaluate performance of classifier
+    E = nist_eval('my_rep2', w, num_test);
+    
+    errors(i) = E;
+end
+
+disp(errors);
